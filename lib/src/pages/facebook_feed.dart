7f37a4ab45f8586dfacab57476665bb1d6644b60
Chunlee Thong <chunlee.thong@gmail.com>
2022-06-29 20:16:41 +0700

    move widgets

--- lib/src/pages/facebook_feed.dart
+++ lib/src/pages/facebook_feed.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const reactionColor = Color(0xFF64686E);
+import '../widgets/reaction_button.dart';
 
 class FacebookFeed extends StatefulWidget {
   const FacebookFeed({Key? key}) : super(key: key);
@@ -200,69 +200,6 @@ class _FacebookPostState extends State<FacebookPost> {
           text: "Share",
         ),
       ],
-    );
-  }
-}
-
-class LikeButton extends StatelessWidget {
-  final IconData icon;
-  final Color color;
-  final VoidCallback onPressed;
-  const LikeButton({Key? key, required this.icon, required this.color, required this.onPressed}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: InkWell(
-        onTap: onPressed,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(icon, color: color, size: 18),
-              const SizedBox(width: 4),
-              Text(
-                "Like",
-                style: TextStyle(color: color),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class OtherReactionButton extends StatelessWidget {
-  final IconData icon;
-  final String text;
-  const OtherReactionButton({
-    Key? key,
-    required this.icon,
-    required this.text,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: InkWell(
-        onTap: () {},
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(icon, color: reactionColor, size: 18),
-              const SizedBox(width: 4),
-              Text(
-                text,
-                style: const TextStyle(color: reactionColor),
-              ),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
